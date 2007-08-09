eb2d638b7893a5396d79b98311ac9410e7e6aac1
ero.carrera <ero.carrera@8842bc4e-7134-0410-8230-5dc5194fb5c1>
2007-08-09 22:52:30 +0000

    pefile-1.2.6

---
+++ tags/pefile-1.2.6/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup, Extension
+import pefile
+
+setup(name = 'pefile',
+    version = pefile.__version__,
+    description = 'Python PE parsing module',
+    author = pefile.__author__,
+    author_email = pefile.__contact__,
+    url = 'http://dkbza.org/pefile.html',
+    platforms = ['any'],
+    classifiers = ['Development Status :: 5 - Production/Stable',
+	'Intended Audience :: Developers',
+	'Intended Audience :: Science/Research',
+	'Natural Language :: English',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python',
+	'Topic :: Software Development :: Libraries :: Python Modules'],
+    long_description = "\n".join(pefile.__doc__.split('\n')),
+    py_modules = ['pefile', 'peutils'])
+
