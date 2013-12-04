b57f4941777cda959353597a4bc55dc6461dd67f
ero.carrera@gmail.com <ero.carrera@gmail.com@8842bc4e-7134-0410-8230-5dc5194fb5c1>
2013-12-04 17:56:58 +0000

    -Added Mandiant's ordlookup to resolve the names for some symbols that are exported only by ordinal numbers. -Added a feature requested in Issue 45 to produce a Python dictionary with all the information dumped by dump_info(). -Fixed a problem introduced when renaming the DLL Characteristics FLAGS that lead to them not being shown. -The parsing of sections will now give up with more than a few errors are encountered, avoiding parsing too many invalid sections in some cases. -Added better constraints to the maximum number of imports that will be parsed.

---
+++ trunk/ordlookup/__init__.py
@@ -0,0 +1,29 @@
+import ws2_32
+import oleaut32
+
+'''
+A small module for keeping a database of ordinal to symbol
+mappings for DLLs which frequently get linked without symbolic
+infoz.
+'''
+
+ords = {
+    'ws2_32.dll':ws2_32.ord_names,
+    'wsock32.dll':ws2_32.ord_names,
+    'oleaut32.dll':oleaut32.ord_names,
+}
+
+def ordLookup(libname, ord):
+    '''
+    Lookup a name for the given ordinal if it's in our
+    database.
+    '''
+    names = ords.get(libname.lower())
+    if names == None:
+        return 'ord%d' % ord
+    name = names.get(ord)
+    if name == None:
+        return 'ord%d' % ord
+    return name
+
+
