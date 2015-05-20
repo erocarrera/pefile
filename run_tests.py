b3d22b7943a9e7da188c377807349fcd32b9f45e
Ero Carrera <ero.carrera@gmail.com>
2015-05-20 20:12:00 +0200

    'python setup.py test' runs the tests now.

--- run_tests.py
+++ run_tests.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 """Script to run the tests."""
 
-import unittest
 import sys
+from unittest import TestLoader, TextTestRunner
 
 # Change PYTHONPATH to include pefile.
 sys.path.insert(0, u'.')
 
 if __name__ == '__main__':
-  test_suite = unittest.TestLoader().discover('.', pattern='*_test.py')
-  test_results = unittest.TextTestRunner(verbosity=2).run(test_suite)
+  test_suite = TestLoader().discover('.', pattern='*_test.py')
+  test_results = TextTestRunner(verbosity=2).run(test_suite)
   if not test_results.wasSuccessful():
     sys.exit(1)
--- setup.py
+++ setup.py
@@ -2,15 +2,31 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from setuptools import setup
+    from setuptools import setup, Command
 except ImportError, excp:
-    from distutils.core import setup
+    from distutils.core import setup, Command
+
+from unittest import TestLoader, TextTestRunner
 
 import pefile
 import os
 
 # os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
 # os.environ['COPYFILE_DISABLE'] = 'true'
+
+class TestCommand(Command):
+  """Run tests."""
+  user_options = []
+
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    pass
+
+  def run(self):
+    test_suite = TestLoader().discover('.', pattern='*_test.py')
+    test_results = TextTestRunner(verbosity=2).run(test_suite)
 
 setup(name = 'pefile',
     version = pefile.__version__,
@@ -29,6 +45,7 @@ setup(name = 'pefile',
     	'Programming Language :: Python',
     	'Topic :: Software Development :: Libraries :: Python Modules'],
     long_description = "\n".join(pefile.__doc__.split('\n')),
+    cmdclass={"test": TestCommand},
     py_modules = ['pefile', 'peutils'],
     packages = ['ordlookup'] )
 
