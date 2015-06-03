64cc13c2062387071792f58002849eec99754723
Ero Carrera <ero.carrera@gmail.com>
2015-06-03 16:53:09 +0200

    --ammend

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup, Command
-except ImportError, excp:
-    from distutils.core import setup, Command
-
+from setuptools import setup, Command
 from unittest import TestLoader, TextTestRunner
 
 import pefile
@@ -25,7 +21,7 @@ class TestCommand(Command):
     pass
 
   def run(self):
-    test_suite = TestLoader().discover('.', pattern='*_test.py')
+    test_suite = TestLoader().discover('tests/', pattern='*_test.py')
     test_results = TextTestRunner(verbosity=2).run(test_suite)
 
 setup(name = 'pefile',
