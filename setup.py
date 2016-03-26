3f06837897185ce12b9f5b18bb1ec547a0570441
Ero Carrera <ero.carrera@gmail.com>
2016-03-26 21:40:48 +0100

    Merged matysek's fix to fetch info from pefile.py without importing it.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
+import ast
 import os
+import re
 import sys
 
 try:
@@ -10,10 +12,34 @@ except ImportError as excp:
 
 from unittest import TestLoader, TextTestRunner
 
-import pefile
 
 os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
 os.environ['COPYFILE_DISABLE'] = 'true'
+
+
+def _read_doc():
+    """
+    Parse docstring from file 'pefile.py' and avoid importing
+    this module directly.
+    """
+    with open('pefile.py', 'r') as f:
+        tree = ast.parse(f.read())
+    return ast.get_docstring(tree)
+
+
+def _read_attr(attr_name):
+    """
+    Parse attribute from file 'pefile.py' and avoid importing
+    this module directly.
+
+    __version__, __author__, __contact__,
+    """
+    regex = attr_name + r"\s+=\s+'(.+)'"
+    with open('pefile.py', 'r') as f:
+        match = re.search(regex, f.read())
+    # Second item in the group is the value of attribute.
+    return match.group(1)
+
 
 class TestCommand(Command):
   """Run tests."""
@@ -39,24 +65,24 @@ class TestCommand(Command):
     test_suite = TestLoader().discover('./tests', pattern='*_test.py')
     test_results = TextTestRunner(verbosity=2).run(test_suite)
 
-setup(
-    name = 'pefile',
-    version = pefile.__version__,
+
+setup(name = 'pefile',
+    version = _read_attr('__version__'),
     description = 'Python PE parsing module',
-    author = pefile.__author__,
-    author_email = pefile.__contact__,
+    author = _read_attr('__author__'),
+    author_email = _read_attr('__contact__'),
     url = 'https://github.com/erocarrera/pefile',
     download_url='https://github.com/erocarrera/pefile/releases/download/v2016.3.4/pefile-2016.3.4.tar.gz',
     keywords = ['pe', 'exe', 'dll', 'pefile', 'pecoff'],
     classifiers = [
         'Development Status :: 5 - Production/Stable',
-    	'Intended Audience :: Developers',
-    	'Intended Audience :: Science/Research',
-    	'Natural Language :: English',
-    	'Operating System :: OS Independent',
-    	'Programming Language :: Python',
-    	'Topic :: Software Development :: Libraries :: Python Modules'],
-    long_description = "\n".join(pefile.__doc__.split('\n')),
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    long_description = "\n".join(_read_doc().split('\n')),
     cmdclass={"test": TestCommand},
     py_modules = ['pefile', 'peutils'],
     packages = ['ordlookup'],
