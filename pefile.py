erocarrera/pefile

commit f5ff78606528d371714e50db567b92cb2eec31b2

Fixed the behavior of is_driver in the case that the import directory hadn't been parsed.

pefile.py
@@ -5233,21 +5233,32 @@ class PE(object):
 
         # This is not reliable either...
         #
-        # if any( (section.Characteristics & SECTION_CHARACTERISTICS['IMAGE_SCN_MEM_NOT_PAGED']) for section in self.sections ):
+        # if any((section.Characteristics &
+        #           SECTION_CHARACTERISTICS['IMAGE_SCN_MEM_NOT_PAGED']) for
+        #        section in self.sections ):
         #    return True
 
-        if hasattr(self, 'DIRECTORY_ENTRY_IMPORT'):
+        # If the import directory was not parsed (fast_load = True); do it now.
+        if not hasattr(self, 'DIRECTORY_ENTRY_IMPORT'):
+            self.parse_data_directories(directories=[
+                DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_IMPORT']])
 
-            # If it imports from "ntoskrnl.exe" or other kernel components it should be a driver
-            #
-            if set( ('ntoskrnl.exe', 'hal.dll', 'ndis.sys', 'bootvid.dll', 'kdcom.dll' ) ).intersection( [ imp.dll.lower() for imp in self.DIRECTORY_ENTRY_IMPORT ] ):
-                return True
+        # self.DIRECTORY_ENTRY_IMPORT will now exist, although it may be empty.
+        # If it imports from "ntoskrnl.exe" or other kernel components it should
+        # be a driver
+        #
+        system_DLLs = set(
+            ('ntoskrnl.exe', 'hal.dll', 'ndis.sys', 'bootvid.dll', 'kdcom.dll'))
+        if system_DLLs.intersection(
+                [imp.dll.lower() for imp in self.DIRECTORY_ENTRY_IMPORT]):
+            return True
 
         return False
 
 
     def get_overlay_data_start_offset(self):
-        """Get the offset of data appended to the file and not contained within the area described in the headers."""
+        """Get the offset of data appended to the file and not contained within
+        the area described in the headers."""
 
         largest_offset_and_size = (0, 0)
 

tests/pefile_test.py
@@ -408,6 +408,20 @@ class Test_pefile(unittest.TestCase):
         self.assertRaises( Exception, pefile.PE, control_file_pe )
 
 
+    def test_driver_check(self):
+        """Test the is_driver check"""
+
+        control_file_pe = os.path.join(
+            REGRESSION_TESTS_DIR,
+            '075356de51afac92d1c20ba53c966fa145172897a96cfdb1b3bb369edb376a77_driver')
+
+        pe_fast = pefile.PE(control_file_pe, fast_load=True)
+        pe_full = pefile.PE(control_file_pe, fast_load=False)
+
+        # Ensure the rebased image is the same as the pre-generated one.
+        self.assertEqual( pe_fast.is_driver(), pe_full.is_driver() )
+
+
     def test_rebased_image(self):
         """Test correctness of rebased images"""
 

commit 8de20cd13ccc723d63ef31443693d2d9e3a25acb

Fix is_driver()
Commit f5ff78606528d371714e50db567b92cb2eec31b2 introduced a regression:
Corrupted (truncated) files where no valid import section could
be found would cause an "has no attribute 'DIRECTORY_ENTRY_IMPORT'"
exception to be raised in is_driver().

pefile.py
@@ -5320,11 +5320,12 @@ class PE(object):
         # If it imports from "ntoskrnl.exe" or other kernel components it should
         # be a driver
         #
-        system_DLLs = set(
-            ('ntoskrnl.exe', 'hal.dll', 'ndis.sys', 'bootvid.dll', 'kdcom.dll'))
-        if system_DLLs.intersection(
-                [imp.dll.lower() for imp in self.DIRECTORY_ENTRY_IMPORT]):
-            return True
+        if hasattr(self, 'DIRECTORY_ENTRY_IMPORT'):
+            system_DLLs = set(
+                ('ntoskrnl.exe', 'hal.dll', 'ndis.sys', 'bootvid.dll', 'kdcom.dll'))
+            if system_DLLs.intersection(
+                    [imp.dll.lower() for imp in self.DIRECTORY_ENTRY_IMPORT]):
+                return True
 
         return False
 