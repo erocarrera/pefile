7750b8daef7aaa84f1ddf6a9b0e4591ef02460b5
Julian Kirsch <kirschju@sec.in.tum.de>
2016-09-15 14:52:01 +0200

    Reflect newest additions to debug types (see for example wine https://github.com/wine-mirror/wine/blob/master/include/winnt.h).

--- pefile.py
+++ pefile.py
@@ -198,7 +198,11 @@ debug_types = [
     ('IMAGE_DEBUG_TYPE_OMAP_FROM_SRC',  8),
     ('IMAGE_DEBUG_TYPE_BORLAND',        9),
     ('IMAGE_DEBUG_TYPE_RESERVED10',     10),
-    ('IMAGE_DEBUG_TYPE_CLSID',          11) ]
+    ('IMAGE_DEBUG_TYPE_CLSID',          11),
+    ('IMAGE_DEBUG_TYPE_VC_FEATURE',     12),
+    ('IMAGE_DEBUG_TYPE_POGO',           13),
+    ('IMAGE_DEBUG_TYPE_ILTCG',          14),
+    ('IMAGE_DEBUG_TYPE_MPX',            15) ]
 
 DEBUG_TYPE = dict([(e[1], e[0]) for e in debug_types]+debug_types)
 
