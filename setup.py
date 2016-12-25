0c90e36184780ef59a4e5c6c552f8b0c8cd5a0a6
Ero Carrera <ero.carrera@gmail.com>
2016-12-25 22:34:01 +0100

    Read with encoding.

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ def _read_doc():
     Parse docstring from file 'pefile.py' and avoid importing
     this module directly.
     """
-    with open('pefile.py', 'r') as f:
-        tree = ast.parse(f.read())
+    with open('pefile.py', 'r', encoding='utf-8') as f:
+        tree = ast.parse(f.read().encode('ascii', 'backslashreplace'))
     return ast.get_docstring(tree)
 
 
@@ -36,7 +36,7 @@ def _read_attr(attr_name):
     __version__, __author__, __contact__,
     """
     regex = attr_name + r"\s+=\s+'(.+)'"
-    with open('pefile.py', 'r') as f:
+    with open('pefile.py', 'r', encoding='utf-8') as f:
         match = re.search(regex, f.read())
     # Second item in the group is the value of attribute.
     return match.group(1)
