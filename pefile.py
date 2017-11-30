458e2d498abffe90eb16c7d57fadfaa351da703c
Antonio Parata <aparata@gmail.com>
2017-11-30 12:15:06 +0100

    Avoid to raise an exception when the type of the _s_ variable is bytes or bytearray.

--- pefile.py
+++ pefile.py
@@ -1432,7 +1432,7 @@ def is_valid_dos_filename(s):
         return False
     # Allow path separators as import names can contain directories.
     allowed = allowed_filename + b'\\/'
-    for c in set(s):
+    for c in set(str(s)):
         if c not in allowed:
             return False
     return True
@@ -1454,7 +1454,7 @@ else:
 def is_valid_function_name(s):
     if s is None or not isinstance(s, (str, bytes, bytearray)):
         return False
-    for c in set(s):
+    for c in set(str(s)):
         if c not in allowed_function_name:
             return False
     return True
