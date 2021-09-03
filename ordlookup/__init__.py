9a642b9919ae4a1eb0b1cd47941e21e0959a94ce
Ero Carrera <ero.carrera@gmail.com>
2021-09-03 16:07:15 +0200

    Ran black on a few other source files, no functional changes

--- ordlookup/__init__.py
+++ ordlookup/__init__.py
@@ -3,33 +3,37 @@ import sys
 from . import ws2_32
 from . import oleaut32
 
-'''
+"""
 A small module for keeping a database of ordinal to symbol
 mappings for DLLs which frequently get linked without symbolic
 infoz.
-'''
+"""
 
 ords = {
-    b'ws2_32.dll': ws2_32.ord_names,
-    b'wsock32.dll': ws2_32.ord_names,
-    b'oleaut32.dll': oleaut32.ord_names,
+    b"ws2_32.dll": ws2_32.ord_names,
+    b"wsock32.dll": ws2_32.ord_names,
+    b"oleaut32.dll": oleaut32.ord_names,
 }
 
 PY3 = sys.version_info > (3,)
 
 if PY3:
+
     def formatOrdString(ord_val):
-        return 'ord{}'.format(ord_val).encode()
+        return "ord{}".format(ord_val).encode()
+
+
 else:
+
     def formatOrdString(ord_val):
-        return b'ord%d' % ord_val
+        return b"ord%d" % ord_val
 
 
 def ordLookup(libname, ord_val, make_name=False):
-    '''
+    """
     Lookup a name for the given ordinal if it's in our
     database.
-    '''
+    """
     names = ords.get(libname.lower())
     if names is None:
         if make_name is True:
