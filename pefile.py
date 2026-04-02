574b8e6b60099196f6d2d78c706068ed333b5417
Ryan Mast <mast9@llnl.gov>
2026-04-02 09:37:24 -0700

    Apply suggestion changing from array to tuple
    Co-authored-by: j-t-1 <120829237+j-t-1@users.noreply.github.com>
    Signed-off-by: Ryan Mast <mast9@llnl.gov>

--- pefile.py
+++ pefile.py
@@ -3477,7 +3477,7 @@ class PE:
                 "IMAGE_SCN_MEM_WRITE", False
             ) and section.__dict__.get("IMAGE_SCN_MEM_EXECUTE", False):
 
-                if section.Name.rstrip(b"\x00") in [b"PAGE", b"INIT"] and self.is_driver():
+                if section.Name.rstrip(b"\x00") in (b"PAGE", b"INIT") and self.is_driver():
                     # Drivers can have a PAGE/INIT section with those flags set without
                     # implying that it is malicious
                     pass
